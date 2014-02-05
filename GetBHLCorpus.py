19fd0182cb6c2d73022f447953c02587ce52f319
prashanti <manda.prashanti@gmail.com>
2014-02-05 14:36:59 -0500

    Deleted

--- GetBHLCorpus.py
+++ GetBHLCorpus.py
@@ -33,30 +33,76 @@ def getVertebrateTitleIDs():
 			titleid=data[0]
 			subject=data[1]
 			if "Vertebrate" in subject:
-				print "yes"
 				vertebratetitleids.add(titleid.strip())
 
+def getVertebratePartIDs():
+	global vertebratepartids
+	inp=open('part_parsed.xls','r')
+	#PartID	ItemID	ContributorName	SequenceOrder	SegmentType	Title	ContainerTitle	PublicationDetails	Volume	Series	Issue	Date	PageRange	StartPageID	LanguageName	SegmentUrl	ExternalUrl	DownloadUrl	
+	for line in inp:
+		if "PartID" not in line:
+			data=line.split("\t")
+			partid=data[0].strip()
+			title=data[2].strip()
+			if "Vertebrate" in title:
+				vertebratepartids.add(partid)
+
+
+def getPageIDsforPartId():
+	global vertebratepartids
+	global partid2pageids_dict
+	for partid in vertebratepartids:
+		url="http://www.biodiversitylibrary.org/api2/httpquery.ashx?op=GetPartMetadata&partid="+partid+"&pages=t&ocr=t&apikey=8c118b05-3e6e-4ef2-92c5-78610a868a14&format=json"
+		content = urllib2.urlopen(url).read()
+		raw = nltk.clean_html(content)
+		decoded_data = json.loads(raw)
+		# keep a set of pageIDs for each partid
+		for x in decoded_data['Result']['Pages']:
+			if partid not in partid2pageids_dict:
+				partid2pageids_dict[partid]=set()
+			partid2pageids_dict[partid].add(x['PageID'])
+			
+	
+
+
 def main():
+	subjectlimit=0
+	partlimit=10
 	global titleids
 	global itemids_dict
 	getTitleIDs()
 	getItemIDs()
 	getVertebrateTitleIDs()
-	f=open('BHLCorpus.txt','w')
-
-	for titleid in vertebratetitleids:
-		f.write("TitleID:  "+str(titleid)+"\n")
-		for item in itemids_dict[titleid]:
-			f.write("ItemID:  "+str(item)+"\n")
-			url="http://www.biodiversitylibrary.org/api2/httpquery.ashx?op=GetItemMetadata&itemid="+item.strip()+"&pages=t&ocr=t&parts=f&apikey=8c118b05-3e6e-4ef2-92c5-78610a868a14&format=json"
-			content = urllib2.urlopen(url).read()
-			raw = nltk.clean_html(content)
-			decoded_data = json.loads(raw)
-			for page in decoded_data['Result']['Pages']:
-				f.write(page['OcrText'].encode('utf-8').strip()+"\n")
-		
-
+	getVertebratePartIDs()
+	getPageIDsforPartId()
 	
+	f=open('BHLCorpus.txt','w')
+	for titleid in vertebratetitleids:
+			for item in itemids_dict[titleid]:
+				if subjectlimit >0:
+					f.write("TitleID:  "+str(titleid)+"\n")
+					subjectlimit=subjectlimit-1
+					f.write("ItemID:  "+str(item)+"\n")
+					url="http://www.biodiversitylibrary.org/api2/httpquery.ashx?op=GetItemMetadata&itemid="+item.strip()+"&pages=t&ocr=t&parts=f&apikey=8c118b05-3e6e-4ef2-92c5-78610a868a14&format=json"
+					content = urllib2.urlopen(url).read()
+					raw = nltk.clean_html(content)
+					decoded_data = json.loads(raw)
+					for page in decoded_data['Result']['Pages']:
+						f.write(page['OcrText'].encode('utf-8').strip()+"\n")
+	
+	f.write("Getting Parts which match Vertebrate in the title")
+					
+	for partid in partid2pageids_dict:
+		if partlimit>0:
+			partlimit=partlimit-1
+			f.write("Part ID: "+str(partid)+"\n")
+			for pageid in partid2pageids_dict[partid]:
+				url ="http://www.biodiversitylibrary.org/api2/httpquery.ashx?op=GetPageOcrText&pageid="+str(pageid)+"&apikey=8c118b05-3e6e-4ef2-92c5-78610a868a14&format=json"
+				content = urllib2.urlopen(url).read()
+				raw = nltk.clean_html(content)
+				decoded_data = json.loads(raw)
+				f.write(decoded_data['Result'].encode('utf-8'))
+
 
 if __name__ == "__main__":
 	import json
@@ -66,6 +112,8 @@ if __name__ == "__main__":
 	titleids=set()
 	itemids_dict=dict()
 	vertebratetitleids=set()
+	vertebratepartids=set()
+	partid2pageids_dict=dict()
 	main()
 
 
